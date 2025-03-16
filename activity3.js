67b82f55108462c4b9f84c692a92a6f6ef1deefc
Sfaith-doctolero <stephjavinar@gmail.com>
2025-03-16 13:53:58 +0800

    Complete

--- activity3.js
+++
@@ -1,7 +0,0 @@
-// Boolean Actvity
-let isMonday = false;
-let isTuesday = true;
-
-console.log("It is Not Monday:", !isMonday);  
-console.log("It is Monday AND Tuesday:", isMonday && isTuesday);  
-console.log("It is Monday OR Tuesday:", isMonday || isTuesday);
